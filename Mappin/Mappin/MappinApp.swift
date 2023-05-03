74bbe243fc1a08e83c1bafe96674be745ba772f9
pccommen <pccommen@gmail.com>
2023-05-03 16:26:03 +0900

    init

---
+++ Mappin/Mappin/MappinApp.swift
@@ -0,0 +1,17 @@
+//
+//  MappinApp.swift
+//  Mappin
+//
+//  Created by changgyo seo on 2023/05/03.
+//
+
+import SwiftUI
+
+@main
+struct MappinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
