ff76ce94cd69fc26551e78a0150f3a2c9adf9f86
sozohoy <sozohoy@gmail.com>
2023-05-04 17:43:40 +0900

    feat: MusicChartUseCase 구현

---
+++ Mappin/Mappin/Domain/UseCases/MusicUseCases/MusicChartUseCase.swift
@@ -0,0 +1,26 @@
+//
+//  MusicChartUseCase.swift
+//  Mappin
+//
+//  Created by 한지석 on 2023/05/04.
+//
+
+import Foundation
+
+protocol MusicChartUseCase {
+    func execute() async throws -> [Music]
+}
+
+final class DefaultMusicChartUseCase: MusicChartUseCase {
+    
+    let musicRepository: RequestMusicRepositoryInterface
+    
+    init(musicRepository: RequestMusicRepositoryInterface) {
+        self.musicRepository = musicRepository
+    }
+    
+    func execute() async throws -> [Music] {
+        return try await musicRepository.requestMusicChart()
+    }
+    
+}
