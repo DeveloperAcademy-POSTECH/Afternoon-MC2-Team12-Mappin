e1b8eac52463df0526867460dc016cce86ac5e4f
sozohoy <sozohoy@gmail.com>
2023-05-04 16:17:29 +0900

    feat: SearchMusicRepository 구현
    MusicKit을 통한 통신으로 유저가 입력하는 노래 / 가수에 대한 응답을 처리합니다.

--- Mappin/Mappin/Data/Repositories/RequestSearchMusicRepository.swift
+++ Mappin/Mappin/Data/Repositories/RequestSearchMusicRepository.swift
@@ -10,25 +10,25 @@ import MusicKit
 
 final class RequestSearchMusicRepository: RequestSearchMusicRepositoryInterface {
     
+    /// 검색한 음악의 응답을 처리합니다.
+    ///  - Parameter searchTerm: music title or artistName
     func requestSearchMusic(searchTerm: String) async throws -> [Music] {
         var searchRequest = MusicCatalogSearchRequest(term: searchTerm, types: [Song.self])
         searchRequest.limit = 10
         do {
             let searchResponse = try await searchRequest.response()
-            var music: [Music] = []
-            for i in 0..<searchResponse.songs.count {
-                music.append(Music(id: searchResponse.songs[i].id, title: searchResponse.songs[i].title, artist: searchResponse.songs[i].artistName, artwork: searchResponse.songs[i].artwork, appleMusicUrl: searchResponse.songs[i]))
+            let music = searchResponse.songs.map {
+                return Music(id: String(describing: $0.id),
+                             title: $0.title,
+                             artist: $0.artistName,
+                             artwork: String(describing: $0.artwork?.url(width: 55, height: 55)),
+                             appleMusicUrl: $0.url)
             }
-            return [Music(id: "1",
-                          title: "1",
-                          artist: "1",
-                          artwork: "1",
-                          appleMusicUrl: URL(string: "www.naver.com")!)]
+            return music
         } catch {
             print("Error")
             return []
         }
     }
-    
     
 }
