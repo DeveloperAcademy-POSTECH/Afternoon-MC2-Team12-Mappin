b52f02ad7e0de9cd88f1cfcf68922b027876666e
sozohoy <sozohoy@gmail.com>
2023-05-07 18:13:56 +0900

    feat: musicReducer 구현

--- Mappin/Mappin/Data/Repositories/RequestMusicRepository.swift
+++ Mappin/Mappin/Data/Repositories/RequestMusicRepository.swift
@@ -17,39 +17,29 @@ final class RequestMusicRepository: RequestMusicRepositoryInterface {
     ) async throws -> [Music] {
         var searchRequest = MusicCatalogSearchRequest(term: searchTerm, types: [Song.self])
         searchRequest.limit = 10
-        do {
-            let searchResponse = try await searchRequest.response()
-            let music = searchResponse.songs.map {
-                return Music(id: String(describing: $0.id),
-                             title: $0.title,
-                             artist: $0.artistName,
-                             artwork: String(describing: $0.artwork?.url(width: 55, height: 55)),
-                             appleMusicUrl: $0.url)
-            }
-            return music
-        } catch {
-            print("Error")
-            return []
+        let searchResponse = try await searchRequest.response()
+        let music = searchResponse.songs.map {
+            return Music(id: String(describing: $0.id),
+                         title: $0.title,
+                         artist: $0.artistName,
+                         artwork: String(describing: $0.artwork?.url(width: 55, height: 55)),
+                         appleMusicUrl: $0.url)
         }
+        return music
     }
     
     func requestMusicChart() async throws -> [Music] {
         var chartRequest = MusicCatalogChartsRequest(types: [Song.self])
         chartRequest.limit = 10
-        do {
-            let chartResponse = try await chartRequest.response().songCharts[0].items
-            let music = chartResponse.map {
-                return Music(id: String(describing: $0.id),
-                             title: $0.title,
-                             artist: $0.artistName,
-                             artwork: String(describing: $0.artwork?.url(width: 55, height: 55)),
-                             appleMusicUrl: $0.url)
-            }
-            return music
-        } catch {
-            print("error")
-            return []
+        let chartResponse = try await chartRequest.response().songCharts[0].items
+        let music = chartResponse.map {
+            return Music(id: String(describing: $0.id),
+                         title: $0.title,
+                         artist: $0.artistName,
+                         artwork: String(describing: $0.artwork?.url(width: 55, height: 55)),
+                         appleMusicUrl: $0.url)
         }
+        return music
     }
     
 }
