860c1b83a141779532227e83744b4b71840ba83a
sozohoy <sozohoy@gmail.com>
2023-05-08 16:55:41 +0900

    feat: MusicItemCollection, String Extension

---
+++ Mappin/Mappin/Common/Extension/String+.swift
@@ -0,0 +1,15 @@
+//
+//  String+.swift
+//  Mappin
+//
+//  Created by 한지석 on 2023/05/08.
+//
+
+import Foundation
+
+extension String {
+    func resizeArtwork(size: Int) -> String {
+        let size = String(format: "%.0fx%.0f", size, size)
+        return replacingOccurrences(of: "99x99", with: size)
+    }
+}
