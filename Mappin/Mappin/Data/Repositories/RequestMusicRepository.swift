a17a61a606c76edbe0510d5d9a45d37d69f03419
sozohoy <sozohoy@gmail.com>
2023-05-08 16:56:19 +0900

    feat: songs map 로직 extension으로 처리

--- Mappin/Mappin/Data/Repositories/RequestMusicRepository.swift
+++ Mappin/Mappin/Data/Repositories/RequestMusicRepository.swift
@@ -6,40 +6,42 @@
 //
 
 import Foundation
+
 import MusicKit
 
 final class RequestMusicRepository: RequestMusicRepositoryInterface {
 
     /// 검색한 음악의 응답을 처리합니다.
     ///  - Parameter searchTerm: music title or artistName
+    ///  return: Music Entities(List)
     func requestSearchMusic(
         searchTerm: String
     ) async throws -> [Music] {
         var searchRequest = MusicCatalogSearchRequest(term: searchTerm, types: [Song.self])
         searchRequest.limit = 10
         let searchResponse = try await searchRequest.response()
-        let music = searchResponse.songs.map {
-            return Music(id: String(describing: $0.id),
-                         title: $0.title,
-                         artist: $0.artistName,
-                         artwork: String(describing: $0.artwork?.url(width: 55, height: 55)),
-                         appleMusicUrl: $0.url)
-        }
-        return music
+        return searchResponse.songs.songToMusic()
     }
     
+    /// 음악을 검색하지 않았을 경우, 보여줄 차트(~10위)를 호출합니다.
+    /// return: Music Entities(List)
     func requestMusicChart() async throws -> [Music] {
         var chartRequest = MusicCatalogChartsRequest(types: [Song.self])
         chartRequest.limit = 10
         let chartResponse = try await chartRequest.response().songCharts[0].items
-        let music = chartResponse.map {
-            return Music(id: String(describing: $0.id),
-                         title: $0.title,
-                         artist: $0.artistName,
-                         artwork: String(describing: $0.artwork?.url(width: 55, height: 55)),
-                         appleMusicUrl: $0.url)
-        }
-        return music
+        return chartResponse.songToMusic()
     }
     
 }
+
+/// 유저가 애플 뮤직에서 음악을 재생할 수 있도록 이동합니다.
+/// return: void
+//    func openAppleMusic(url: URL?) {
+//        guard let appleMusicUrl = url,
+//              UIApplication.shared.canOpenURL(appleMusicUrl)
+//        else {
+//            print("URL이 없는 음악이거나, URL을 열 수 없음.")
+//            return
+//        }
+//        UIApplication.shared.open(appleMusicUrl)
+//    }
