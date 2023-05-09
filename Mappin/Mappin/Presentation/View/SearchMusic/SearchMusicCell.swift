ca2694e3553f26a7982e296e253e5083117eddb8
sozohoy <sozohoy@gmail.com>
2023-05-09 15:28:44 +0900

    add: Presentation - SearchMusicView, Cell

---
+++ Mappin/Mappin/Presentation/View/SearchMusic/SearchMusicCell.swift
@@ -0,0 +1,20 @@
+//
+//  MusicListCell.swift
+//  Mappin
+//
+//  Created by 한지석 on 2023/05/09.
+//
+
+import SwiftUI
+
+struct SearchMusicCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MusicListCell_Previews: PreviewProvider {
+    static var previews: some View {
+        SearchMusicCell()
+    }
+}
---
+++ Mappin/Mappin/Presentation/View/SearchMusic/SearchMusicView.swift
@@ -0,0 +1,20 @@
+//
+//  SearchMusicView.swift
+//  Mappin
+//
+//  Created by 한지석 on 2023/05/09.
+//
+
+import SwiftUI
+
+struct SearchMusicView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SearchMusicView_Previews: PreviewProvider {
+    static var previews: some View {
+        SearchMusicView()
+    }
+}
