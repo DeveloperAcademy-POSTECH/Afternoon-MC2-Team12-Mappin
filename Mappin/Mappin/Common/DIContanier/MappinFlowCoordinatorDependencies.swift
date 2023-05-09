562ab9d898aa8928afa0eaba797bf813c0533064
pccommen <pccommen@gmail.com>
2023-05-09 21:42:27 +0900

    Make DIContainer

---
+++ Mappin/Mappin/Common/DIContanier/MappinFlowCoordinatorDependencies.swift
@@ -0,0 +1,21 @@
+//
+//  MappinFlowCoordinatorDependencies.swift
+//  Mappin
+//
+//  Created by changgyo seo on 2023/05/09.
+//
+
+import Foundation
+import SwiftUI
+
+protocol MappinFlowCoordinatorDependencies {
+    
+//    @ViewBuilder
+//    func makeLaunchView() -> View
+//
+//    @ViewBuilder
+//    func makeMappinView() -> View
+//
+//    @ViewBuilder
+//    func makeArchiveView() -> View
+}
---
+++ Mappin/Mappin/Common/DIContanier/MappinSceneDIContainer.swift
@@ -0,0 +1,12 @@
+//
+//  MappinSceneDIContainer.swift
+//  Mappin
+//
+//  Created by changgyo seo on 2023/05/09.
+//
+
+import Foundation
+
+final class MappinSceneDIContainer: MappinFlowCoordinatorDependencies {
+    
+}
