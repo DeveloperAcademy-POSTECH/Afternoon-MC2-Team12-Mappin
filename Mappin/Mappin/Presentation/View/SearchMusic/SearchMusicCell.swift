fe588bdf950e260f35c0bbe66977cc188b9d3831
sozohoy <sozohoy@gmail.com>
2023-05-09 16:25:23 +0900

    feat: SearchMusicView & Cell 구현 및 데이터 연결

--- Mappin/Mappin/Presentation/View/SearchMusic/SearchMusicCell.swift
+++ Mappin/Mappin/Presentation/View/SearchMusic/SearchMusicCell.swift
@@ -8,13 +8,48 @@
 import SwiftUI
 
 struct SearchMusicCell: View {
+    
+    let music: Music
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            if let existingArtwork = music.artwork {
+                VStack {
+                    Spacer()
+                    AsyncImage(url: existingArtwork) { image in
+                        image.image?.resizable()
+                            .frame(width: 55, height: 55)
+                        
+                    }
+                    Spacer()
+                }
+            }
+            VStack(alignment: .leading) {
+                Text(music.title)
+                    .font(.system(size: 15, weight: .regular))
+                    .lineLimit(1)
+                    .foregroundColor(.primary)
+                if !music.artist.isEmpty {
+                    Text("노래 ・ \(music.artist)")
+                        .lineLimit(1)
+                        .foregroundColor(.secondary)
+                        .padding(.top, -4.0)
+                }
+            }
+            Spacer()
+            Button {
+                
+            } label: {
+                Image(systemName: "plus")
+                    .foregroundColor(.black)
+            }
+
+        }
     }
 }
 
-struct MusicListCell_Previews: PreviewProvider {
-    static var previews: some View {
-        SearchMusicCell()
-    }
-}
+//struct MusicListCell_Previews: PreviewProvider {
+//    static var previews: some View {
+//        SearchMusicCell()
+//    }
+//}
--- Mappin/Mappin/Presentation/View/SearchMusic/SearchMusicView.swift
+++ Mappin/Mappin/Presentation/View/SearchMusic/SearchMusicView.swift
@@ -7,14 +7,108 @@
 
 import SwiftUI
 
+import ComposableArchitecture
+import MusicKit
+
 struct SearchMusicView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    
+    @State private var searchTerm: String = ""
+    
+    let store: StoreOf<MusicReducer>
+    @ObservedObject var viewStore: ViewStoreOf<MusicReducer>
+    
+    init(store: StoreOf<MusicReducer>) {
+        self.store = store
+        self.viewStore = ViewStore(self.store, observe: { $0 })
     }
+    
+    var body: some View {
+        VStack {
+            titleWithCancel
+            searchBar
+            searchMusicList
+        }
+        .onAppear {
+            settingMuesicAuthorization()
+        }
+        .task {
+            viewStore.send(.requestMusicChart)
+        }
+    }
+    
+    /// 최상단 뷰(타이틀, 취소버튼) 구현
+    var titleWithCancel: some View {
+        HStack {
+            Text("현재 위치에 음악 핀하기")
+                .font(.system(size: 16,
+                              weight: .bold))
+                .padding(.leading, 16)
+            Spacer()
+            Button {
+                
+            } label: {
+                Text("취소")
+                    .font(.system(size: 16,
+                                  weight: .regular))
+                    .foregroundColor(.black)
+                    .padding(.trailing, 16)
+            }
+        }
+        .padding(.top, 19)
+    }
+    
+    /// SearchBar 구현
+    var searchBar: some View {
+        HStack {
+            HStack {
+                Image(systemName: "magnifyingglass")
+                TextField("Search", text: viewStore.binding(get: \.searchTerm,
+                                                            send: MusicReducer.Action.searchTermChanged))
+                    .foregroundColor(.primary)
+                    .frame(height: 36)
+                if !viewStore.searchTerm.isEmpty {
+                    Button(action: {
+                        viewStore.send(.resetSearchTerm)
+                        viewStore.send(.requestMusicChart)
+                    }) {
+                        Image(systemName: "xmark.circle.fill")
+                    }
+                } else {
+                    EmptyView()
+                }
+                
+            }
+            .padding(EdgeInsets(top: 0, leading: 8, bottom: 0, trailing: 8))
+            .foregroundColor(Color(red: 0.5569,
+                                   green: 0.5569,
+                                   blue: 0.5765))
+            .background(Color(red: 0.9216,
+                              green: 0.9216,
+                              blue: 0.9412))
+            .cornerRadius(10.0)
+        }
+        .padding(.horizontal)
+    }
+    
+    var searchMusicList: some View {
+        List(viewStore.music) { music in
+            SearchMusicCell(music: music)
+        }
+        .scrollContentBackground(.hidden)
+    }
+    
+    func settingMuesicAuthorization() {
+        Task {
+            _ = await MusicAuthorization.request()
+        }
+    }
+    
 }
 
-struct SearchMusicView_Previews: PreviewProvider {
-    static var previews: some View {
-        SearchMusicView()
-    }
-}
+
+
+//struct SearchMusicView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        SearchMusicView()
+//    }
+//}
