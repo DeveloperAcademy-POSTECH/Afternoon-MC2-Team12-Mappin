38745333395f4aebb42fb39915c053954633f0d7
sozohoy <sozohoy@gmail.com>
2023-05-09 16:31:29 +0900

    add: ArchiveMusic

---
+++ Mappin/Mappin/Presentation/View/ArchiveMusic/ArchiveMusicView.swift
@@ -0,0 +1,20 @@
+//
+//  ArchiveMusicView.swift
+//  Mappin
+//
+//  Created by 한지석 on 2023/05/09.
+//
+
+import SwiftUI
+
+struct ArchiveMusicView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ArchiveMusicView_Previews: PreviewProvider {
+    static var previews: some View {
+        ArchiveMusicView()
+    }
+}
