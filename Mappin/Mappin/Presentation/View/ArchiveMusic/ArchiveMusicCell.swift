cf9bae743d491d8ac68f6f5695a6ce89200a26c0
sozohoy <sozohoy@gmail.com>
2023-05-11 18:03:26 +0900

    feat: 아카이브 페이지 구현

--- Mappin/Mappin/Presentation/View/ArchiveMusic/ArchiveMusicCell.swift
+++ Mappin/Mappin/Presentation/View/ArchiveMusic/ArchiveMusicCell.swift
@@ -8,13 +8,54 @@
 import SwiftUI
 
 struct ArchiveMusicCell: View {
+    var music: Music
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            HStack {
+                Text("Apr 11, 2023 ・ 6:03PM ・ 화요일")
+                    .font(.system(size: 15, weight: .semibold))
+                    .foregroundColor(Color(red: 0.4235, green: 0.4235, blue: 0.4392))
+                Spacer()
+            }
+            
+            HStack(spacing: 0) {
+                Text("")
+                    .frame(width: 0, height: 0)
+                Rectangle()
+                    .frame(width: 55, height: 55)
+                    .foregroundColor(Color(uiColor: .systemGray4))
+                    .overlay {
+                        if let existingArtwork = music.artwork {
+                            AsyncImage(url: existingArtwork) { image in
+                                image
+                                    .resizable()
+                                    .frame(width: 55, height: 55)
+                            } placeholder: {
+                                ProgressView()
+                                    .frame(width: 55, height: 55)
+                            }
+                        }
+                    }
+                    .cornerRadius(8)
+                    .padding(.trailing, 10)
+
+                VStack(alignment: .leading) {
+                    Text(music.title)
+                        .font(.system(size: 15, weight: .regular))
+                        .lineLimit(1)
+                        .foregroundColor(.primary)
+                        .padding(.bottom, -5)
+                    if !music.artist.isEmpty {
+                        Text("노래 ・ \(music.artist)")
+                            .lineLimit(1)
+                            .foregroundColor(.secondary)
+                    }
+                }
+                Spacer()
+            }
+        }
+
     }
 }
 
-struct ArchiveMusicCell_Previews: PreviewProvider {
-    static var previews: some View {
-        ArchiveMusicCell()
-    }
-}
--- Mappin/Mappin/Presentation/View/ArchiveMusic/ArchiveMusicView.swift
+++ Mappin/Mappin/Presentation/View/ArchiveMusic/ArchiveMusicView.swift
@@ -7,14 +7,81 @@
 
 import SwiftUI
 
-struct ArchiveMusicView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
+import MusicKit
+import ComposableArchitecture
 
-struct ArchiveMusicView_Previews: PreviewProvider {
-    static var previews: some View {
-        ArchiveMusicView()
+struct ArchiveMusicView: View {
+    
+    @State var tempButton = false
+    let store: StoreOf<MusicReducer>
+    @ObservedObject var viewStore: ViewStoreOf<MusicReducer>
+    
+    init(store: StoreOf<MusicReducer>) {
+        self.store = store
+        self.viewStore = ViewStore(self.store, observe: { $0 })
     }
+    
+    var body: some View {
+        NavigationView {
+            VStack {
+                if tempButton {
+                    archiveEmptyView
+                } else {
+                    archiveMusicList
+                }
+            }
+            .navigationBarTitle("", displayMode: .inline)
+            .navigationBarItems(leading:
+                                    Text("내가 저장한 핀들 돌아보기")
+                                        .font(.system(size: 16, weight: .bold)),
+                                trailing:
+                                    Button(action: {
+                                        print("취소 버튼 클릭")
+                                    tempButton.toggle()
+                                    }, label: {
+                                        Text("취소")
+                                            .font(.system(size: 16, weight: .regular))
+                                            .foregroundColor(.black)
+                                    }))
+            .onAppear {
+                settingMuesicAuthorization()
+            }
+            .task {
+                viewStore.send(.requestMusicChart)
+                // 서버 API 호출
+            }
+        }
+    }
+    
+    /// 음악 검색 리스트 구현
+    var archiveMusicList: some View {
+        withAnimation {
+            List {
+                Section {
+                    ForEach(!viewStore.searchTerm.isEmpty ? viewStore.searchMusic : viewStore.musicChart) { music in
+                         // selectedMusicIndex == "" -> 초기 상태, 검색했거나 검색창을 켰을 경우. checkmark와 이중 클릭 확인을 하기 위함
+                         // 초기 상태, 혹은 유저가 검색을 했을 때. opacity를 주기 위함
+                        ArchiveMusicCell(music: music)
+                            .onTapGesture {
+                        }
+                    }
+                    // onDelete
+                } header: {
+                }
+
+            }
+            .listStyle(.inset)
+        }
+    }
+    
+    var archiveEmptyView: some View {
+        Text("아카이브가 비어있어요")
+    }
+    
+    func settingMuesicAuthorization() {
+        Task {
+            _ = await MusicAuthorization.request()
+        }
+    }
+    
 }
