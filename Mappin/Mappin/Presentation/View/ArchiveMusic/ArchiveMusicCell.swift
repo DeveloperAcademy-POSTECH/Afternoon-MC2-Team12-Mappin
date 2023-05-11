b3d8c75adb97b48ce147fb7012cecdf57b3efe90
sozohoy <sozohoy@gmail.com>
2023-05-11 17:59:44 +0900

    add: ArchiveMusicCell 파일 추가

---
+++ Mappin/Mappin/Presentation/View/ArchiveMusic/ArchiveMusicCell.swift
@@ -0,0 +1,20 @@
+//
+//  ArchiveMusicCell.swift
+//  Mappin
+//
+//  Created by 한지석 on 2023/05/11.
+//
+
+import SwiftUI
+
+struct ArchiveMusicCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ArchiveMusicCell_Previews: PreviewProvider {
+    static var previews: some View {
+        ArchiveMusicCell()
+    }
+}
