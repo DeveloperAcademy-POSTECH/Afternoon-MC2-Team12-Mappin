bd8c342abe6ad24acf87c570312b8eefaaf3c744
sozohoy <sozohoy@gmail.com>
2023-05-14 23:27:46 +0900

    feat: 디자인 수정, uploadMusic 오류 해결

--- Mappin/Mappin/Presentation/View/SearchMusic/SearchMusicView.swift
+++ Mappin/Mappin/Presentation/View/SearchMusic/SearchMusicView.swift
@@ -23,23 +23,33 @@ struct SearchMusicView: View {
     var body: some View {
         NavigationView {
             VStack {
+                searchBar
+                    .padding(.bottom, 15)
+                Divider()
                 searchMusicList
             }
-            .navigationBarTitle("", displayMode: .inline)
+            .navigationBarTitle("현재 위치에 음악 핀하기", displayMode: .inline)
             .navigationBarItems(leading:
-                                    Text("현재 위치에 음악 핀하기")
-                                        .font(.system(size: 16, weight: .bold)),
-                                trailing:
                                     Button(action: {
                                         viewStore.send(.searchMusicPresent(isPresented: false))
                                     }, label: {
                                         Text("취소")
-                                            .font(.system(size: 16, weight: .regular))
-                                            .foregroundColor(.black)
-                                    }))
-            .searchable(text: viewStore.binding(get: \.searchTerm, send: SearchMusicReducer.Action.searchTermChanged),
-                        placement: .navigationBarDrawer(displayMode: .always) )
-
+                                            .font(.system(size: 16, weight: .bold))
+                                            .foregroundColor(.red)
+                                    }),
+                                trailing:
+                                    Button(action: {
+                                        viewStore.send(.uploadMusic)
+                                    }, label: {
+                                        Text("추가")
+                                            .font(.system(size: 16, weight: viewStore.selectedMusicIndex == "" ? .regular : .bold))
+                                            .foregroundColor(viewStore.selectedMusicIndex == "" ? .gray : .accentColor)
+                                    })
+                                        .disabled(viewStore.selectedMusicIndex == "")
+                                        .onAppear(perform: {
+                                            print("@Kozi - \(viewStore.selectedMusicIndex)")
+                                        })
+            )
             .onAppear {
                 settingMuesicAuthorization()
                 print("@Kozi - \(MusicAuthorization.currentStatus)")
@@ -50,33 +60,52 @@ struct SearchMusicView: View {
         }
     }
     
+    var searchBar: some View {
+         HStack {
+             HStack {
+                 Image(systemName: "magnifyingglass")
+                 TextField("Search", text: viewStore.binding(get: \.searchTerm,
+                                                             send: SearchMusicReducer.Action.searchTermChanged))
+                     .foregroundColor(.primary)
+                     .frame(height: 36)
+                 if !viewStore.searchTerm.isEmpty {
+                     Button(action: {
+                         viewStore.send(.resetSearchTerm)
+                     }) {
+                         Image(systemName: "xmark.circle.fill")
+                     }
+                 } else {
+                     EmptyView()
+                 }
+
+             }
+             .padding(EdgeInsets(top: 0, leading: 8, bottom: 0, trailing: 8))
+             .foregroundColor(Color(red: 0.5569,
+                                    green: 0.5569,
+                                    blue: 0.5765))
+             .background(Color(red: 0.9216,
+                               green: 0.9216,
+                               blue: 0.9412))
+             .cornerRadius(10.0)
+         }
+         .padding(.horizontal)
+     }
+    
     /// 음악 검색 리스트 구현
     var searchMusicList: some View {
         withAnimation {
             List {
-                Section {
-                    ForEach(!viewStore.searchTerm.isEmpty ? viewStore.searchMusic : viewStore.musicChart) { music in
-                        let isSelected = viewStore.selectedMusicIndex == music.id // selectedMusicIndex == "" -> 초기 상태, 검색했거나 검색창을 켰을 경우. checkmark와 이중 클릭 확인을 하기 위함
-                        let noSelection = viewStore.selectedMusicIndex.isEmpty // 초기 상태, 혹은 유저가 검색을 했을 때. opacity를 주기 위함
-                        SearchMusicCell(music: music, isSelected: isSelected, noSelection: noSelection)
-                            .onTapGesture {
-                                if isSelected {
-<<<<<<< Updated upstream
-                                    viewStore.send(.uploadMusic)
-=======
-                                    parent.passMusic()
-//                                    viewStore.send(.uploadMusic)
->>>>>>> Stashed changes
-                                } else {
-                                    viewStore.send(.musicSelected(music.id))
-                                }
-                        }
+                ForEach(!viewStore.searchTerm.isEmpty ? viewStore.searchMusic : viewStore.musicChart) { music in
+                    let isSelected = viewStore.selectedMusicIndex == music.id // selectedMusicIndex == "" -> 초기 상태, 검색했거나 검색창을 켰을 경우. checkmark와 이중 클릭 확인을 하기 위함
+                    SearchMusicCell(music: music, isSelected: isSelected)
+                        .onTapGesture {
+                            if isSelected {
+                                viewStore.send(.musicCanceled)
+                            } else {
+                                viewStore.send(.musicSelected(music.id))
+                            }
                     }
-                } header: {
-                    Text(viewStore.searchTerm.isEmpty ? "현재 이 지역 음악 추천" : "검색 결과")
-                        .padding(.leading, 15)
                 }
-
             }
             .listStyle(.inset)
         }
