03c2ea0e2ec107ca501bfb73fa0f2febc517fbc9
sozohoy <sozohoy@gmail.com>
2023-05-15 01:09:10 +0900

    feat: 시리 구현 완료

--- Mappin/Mappin/Common/Intent/AddPinIntent.swift
+++ Mappin/Mappin/Common/Intent/AddPinIntent.swift
@@ -7,22 +7,33 @@
 
 import AppIntents
 
+
+
 struct AddPinIntent: AppIntent {
     
-    static var title: LocalizedStringResource = "Add pin"
+    static var title: LocalizedStringResource = "pinning"
     static var description = IntentDescription("Let's add our new pin")
     
+//    @IntentParameter(title: "Hi", optionsProvider:)
+//    @IntentParameterSummary
+    
     @IntentParameter(title: "음악")
+    
     var music: String
     
+    // 말을 못알아 듣는다
     func perform() async throws -> some IntentResult {
-        
         let musicSearchUseCase = DefaultSearchMusicUseCase(musicRepository: RequestMusicRepository()) 
-        let list = try await musicSearchUseCase.execute(searchTerm: music).map { $0.title }
-        _ = try await $music.requestDisambiguation(among: list, dialog: "which?")
+        let musicList = try await musicSearchUseCase.execute(searchTerm: music).map {
+            "\($0.artist) - \($0.title)"
+        }
+        let uploadMusic = try await musicSearchUseCase.execute(searchTerm: music)
         
-        
-        return .result()
+        let selectedMusic = try await $music.requestDisambiguation(among: musicList, dialog: "음악을 골라주세요!")
+        let index = musicList.firstIndex { $0 == selectedMusic }
+        try await DefaultMockDIContainer.shared.container.resolver.resolve(AddPinUseCase.self).excute(music: uploadMusic[index!])
+        return .result(value: selectedMusic, dialog: IntentDialog(stringLiteral:
+       "피닝 성공했어요! 감사합니다."))
     }
 }
 
@@ -31,10 +42,12 @@ struct AddPinShortcutProvider: AppShortcutsProvider {
     static var appShortcuts: [AppShortcut] = [
         AppShortcut(
             intent: AddPinIntent(),
-            phrases: ["Add pin in \(.applicationName)"]
+            phrases: ["\(.applicationName) 실행해줘."]
         )
     ]
 }
 
 
 
+//        return .result
+//        return .result
